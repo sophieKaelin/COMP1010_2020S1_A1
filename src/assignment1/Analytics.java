90aa7e003a3b2604d33813b2f879df5cc0a7d8d7
Sophie Kaelin <sophie.kaelin@students.mq.edu.au>
2020-04-07 10:15:14 +1000

    Complete all methods except timeToDisplay

--- src/assignment1/Analytics.java
+++ src/assignment1/Analytics.java
@@ -3,16 +3,26 @@ package assignment1;
 import java.io.FileNotFoundException;
 
 public class Analytics {
+	/**
+	 * An array of strings for the students results combined
+	 * to split it into usable data use:
+	 * for an item in the array at index i,
+	 * String[] tokens = records[i].split(",");
+	 * - tokens[0] = Student ID (numerical value stored as String)
+	 * - tokens[1] = Start Date and Time (format DD/MM/YYYY HH:MM)
+	 * - tokens[2] = End Date and Time (format DD/MM/YYYY HH:MM)
+	 * - tokens[3] = Total Grade (numerical value stored as String)
+	 * @note marks for each question are stored as Strings in tokens[4], tokens[5], ....
+	 * - marks in first question (numerical value stored as String, but could be "-" implying non-attempt): tokens[4]
+	 * - marks in second question (numerical value stored as String, but could be "-" implying non-attempt): tokens[5]
+	 * ...
+	 */
 	public String[] records;
 	public int[] weights;
 
 	public Analytics(String filename) throws FileNotFoundException {
 		records = DataReader.readData(filename);
 		weights = DataReader.getWeights(filename);
-		System.out.println(this.averageQuizPercentageMark());
-		for(int i=0; i < records.length; i++) {
-			System.out.println(getTimeTakenAsStringByIndex(i));
-		}
 	}
 
 	/**
@@ -29,20 +39,11 @@ public class Analytics {
 	 */
 	public int quizTotal() {
 		int total = 0;
-		for(int i = 0; i < weights.length; i++) {
-			total += weights[i];
+		for(int item : weights) {
+			total += item;
 		}
 		return total;
 	}
-
-		public int[] getAttempt(int idx) {
-			String[] tokens = records[idx].split(",");
-			int[] result = new int[weights.length];
-			for(int i=4; i < tokens.length; i++) {
-				result[i-4] = Integer.parseInt(tokens[i]);
-			}
-			return result;
-		}
 
 	/**
 	 * 10 marks
@@ -52,11 +53,26 @@ public class Analytics {
 	 * for example, any value OUTSIDE 1 to 31 is invalid if there are 31 questions.
 	 */
 	public int getQuestionWeight(int questionNumber) {
-		if(questionNumber <= 0 || questionNumber >= countQuestions())	{
+		if(questionNumber <= 0 || questionNumber > countQuestions()) {
 			return 0;
 		}
-		System.out.println("             " + weights[1]);
 		return weights[questionNumber-1];
+	}
+	
+	/**
+	 * DO NOT MODIFY
+	 * PROVIDED AS A SAMPLE
+	 * @return number of students who get a zero
+	 */
+	public int countZeroes() {
+		int count = 0;
+		for(int i=0; i < records.length; i++) {
+			String[] tokens = records[i].split(",");
+			if(Integer.parseInt(tokens[3]) == 0) {
+				count++;
+			}
+		}
+		return count;
 	}
 
 	/**
@@ -66,7 +82,12 @@ public class Analytics {
 	 */
 	public int countPasses(int passMark) {
 		int count = 0;
-
+		for(String record : records) {
+			String[] tokens = record.split(",");
+			if(Integer.parseInt(tokens[3]) >= passMark) {
+				count++;
+			}
+		}
 		return count;
 	}
 
@@ -75,10 +96,13 @@ public class Analytics {
 	 * @return the overall average percentage marks for the quiz. 
 	 * @note the mark for a given attempt is the 4th value in the record
 	 */
-	public double averageQuizPercentageMark() {
-		int total = 0;
-
-		return 0;
+	public double avgQuizPercent() {
+		double count = 0;
+		for(String record: records) {
+			String[] tokens = record.split(",");
+			count += (Integer.parseInt(tokens[3]) / (double)quizTotal());
+		}
+		return count / records.length * 100; //to be completed
 	}
 
 	/**
@@ -87,8 +111,11 @@ public class Analytics {
 	 */
 	public int highestMark() {
 		int highest = 0;
-
-		return highest;
+		for(String record: records) {
+			String[] tokens = record.split(",");
+			highest = Math.max(highest, Integer.parseInt(tokens[3]));
+		}
+		return highest; //to be completed
 	}
 
 	/**
@@ -97,10 +124,16 @@ public class Analytics {
 	 * @return the best mark for the student with given id 
 	 * (return 0 if an attempt for that id doesn't exist)
 	 */
-	public  int bestMarkByStudent(int id) {
+	public int bestMarkByStudent(int id) {
 		int best = 0;
-
-		return best;
+		for(String record: records) {
+			String[] tokens = record.split(",");
+			String ID = String.valueOf(id);
+			if(ID.equals(tokens[0])) {
+				best = Math.max(best, Integer.parseInt(tokens[3]));
+			}
+		}
+		return best; //to be completed
 	}
 
 	/**
@@ -109,8 +142,21 @@ public class Analytics {
 	 * @return the average mark for the student with given id 
 	 * (return 0 if an attempt for that id doesn't exist)
 	 */
-	public double averageMarkByStudent(int id) {
-		return 0;
+	public double avgMarkByStudentId(int id) {
+		double average = 0;
+		int count = 0;
+		for(String record: records) {
+			String[] tokens = record.split(",");
+			String ID = String.valueOf(id);
+			if(ID.equals(tokens[0])) {
+				average += Integer.parseInt(tokens[3]);
+				count++;
+			}
+		}
+		if(count == 0) {
+			return 0;
+		}
+		return average / (double)count; //to be completed
 	}
 
 	/**
@@ -118,9 +164,34 @@ public class Analytics {
 	 * @param questionNumber
 	 * @return the average percentage mark for the given question number.
 	 * if questionNumber = 1, return the average percentage mark for the first question, and so on...
+	 * 
+	 * NOTE: some students might not attempt a particular question, indicated by a dash ("-").
+	 * these values don't contribute towards attempts or average. 
+	 * For example, let the following be the first entries in the array records:
+	 * 
+	 * records[0] = "551102,14/2/18 10:52,14/2/18 11:00,2,1,1"
+	 * records[1] = "595789,14/2/18 12:27,14/2/18 14:08,1,-,1"
+	 * records[2] = "463521,14/2/18 13:26,17/2/18 16:32,1,-,0"
+ 	 * records[3] = "610197,14/2/18 17:04,14/2/18 17:31,0,0,0"
+ 	 * 
+ 	 * If you look at Question 1 (5th comma-separate value), the marks are: "1", "-", "-" and "0".
+ 	 * Thus the average should be, mathematically, (1+0)/2, which should be returned as (maintaining precision) 0.5
 	 */
-	public double averagePercentageMarkByQuestion(int questionNumber) {
-		return 0;
+	public double avgPercentByQuestion(int questionNumber) {
+		if(getQuestionWeight(questionNumber) == 0) {
+			return 0;
+		}
+		int numStudents = 0;
+		int marks = 0;
+		
+		for(String record:records) {
+			String[] tokens = record.split(",");
+			if(!tokens[questionNumber+3].equals("-")) {
+				numStudents ++;
+				marks += Integer.parseInt(tokens[questionNumber+3]);
+			}
+		}	
+		return marks / (double)getQuestionWeight(questionNumber) * 100 / numStudents;
 	}
 
 	/**
@@ -129,20 +200,16 @@ public class Analytics {
 	 * return 1 if the first question was the hardest, 2 if the second was and so on...
 	 */
 	public int hardestQuestion() {
-		int toughest = 1;
-
-		return toughest;
-	}
-
-	/**
-	 * HELPER
-	 * @param data
-	 * @param item
-	 * @return
-	 */
-	private static boolean contains(int[] data, int item) {
-
-		return false;
+		int num = 1;
+		double curLowest = avgPercentByQuestion(1);
+		for(int i = 1; i < weights.length; i++) {
+			double thisLow = avgPercentByQuestion(i+1);
+			if(thisLow < curLowest) {
+				num = i+1;
+				curLowest = thisLow;
+			}
+		}
+		return num; //to be completed
 	}
 
 	/**
@@ -151,9 +218,36 @@ public class Analytics {
 	 * @return an array containing marks of all attempts for student with given id
 	 * return an empty array if an attempt for student with given id doesn't exist.
 	 */
-	public int[] getAllAttemptsForStudent(int id) {
-
-		return null;
+	public int[] getStudentAttempts(int id) {
+		int attempts = numAttempts(id); 
+		if(attempts == 0) {
+			return new int[0];
+		}
+		int[] scores = new int[attempts];
+		int idx = 0;
+		for(String record: records) {
+			String[] tokens = record.split(",");
+			if(tokens[0].equals(String.valueOf(id))) {
+				scores[idx] = Integer.parseInt(tokens[3]);
+				idx++;
+			}
+		}
+		return scores;
+	}
+	
+	/* HELPER FUNCTION
+	 * @param id
+	 * @return number of attempts made by a student
+	 */
+	public int numAttempts(int id) {
+		int attempts = 0;
+		for(String record: records) {
+			String[] tokens = record.split(",");
+			if(tokens[0].contentEquals(String.valueOf(id))) {
+				attempts++;
+			}
+		}
+		return attempts;
 	}
 
 	/**
@@ -162,29 +256,79 @@ public class Analytics {
 	 * @return an array containing question numbers from the hardest to easiest (based on average marks in the questions)
 	 */
 	public int[] getHardestToEasiest() {
-		return null;
+		int[] questions = new int[countQuestions()];
+		for(int i = 0; i < countQuestions(); i++) {
+			questions[i] = i+1;
+		}
+		insertionSort(questions);
+		return questions; //to be completed
 	}
+	
+	/**
+	 * HELPER METHOD
+	 * Insertion Sort
+	 */
+	void insertionSort(int arr[]) { 
+        for (int i = 1; i < arr.length; i++) { 
+            int val = arr[i]; 
+            int j = i - 1; 
+ 
+            while (j >= 0 && avgPercentByQuestion(arr[j]) > avgPercentByQuestion(val)) { 
+                arr[j + 1] = arr[j]; 
+                j = j - 1; 
+            } 
+            arr[j + 1] = val; 
+        } 
+    } 
 
 	/**
 	 * 5 marks
-	 * @param time (in minutes)
+	 * @param idx index in array records for which the time must be returned
+	 * @return time in the formats specified in the JUnit tests
 	 * 
-	 * return time in the formats specified in the JUnit tests
-	 */
-	public String getTimeTakenAsStringByIndex(int idx) {
-		return "Hgjhg";		
-	}
-
-	/**
-	 * HELPER
+	 * there are three components in time (for our purpose) - days, hours and minutes.
+	 * only those components must be included that have a non-zero value
+	 * (the only exception being when the quiz starts and finishes during the same minute,
+	 * in which case it should return "0 minutes").
+	 * a comma separates two components, except the last two, where "and" is the separator.
 	 * 
-	 * @param date
-	 * @param time
-	 * @return number of seconds since 00:00, 01/01/2020
+	 * see JUnit tests for more clarity
+	 * records[0] = "551102,14/2/18 10:52,14/2/18 11:00,2,1,1"
+	 * records[1] = "595789,14/2/18 12:27,14/2/18 14:08,1,-,1"
+	 * records[2] = "463521,14/2/18 13:26,17/2/18 16:32,1,-,0"
+ 	 * records[3] = "610197,14/2/18 17:04,14/2/18 17:31,0,0,0"
+	 * 
 	 */
-	private static int epoch(String[] date, String[] time) {
-		int total = 0;
-
-		return total;
+	public String timeToDisplayByIndex(int idx) {
+		String[] tokens = records[idx].split(",");
+		/* arr[0] = Day
+		 * arr[1] = Month
+		 * arr[2] = Year
+		 * arr[3] = Hour
+		 * arr[4] = Minute
+		 */
+		String[] start = tokens[1].split("/|:| ");
+		String[] end = tokens[2].split("/|:| ");
+		int minutes = Integer.parseInt(end[4]) - Integer.parseInt(start[4]);
+		int hours = Integer.parseInt(end[3]) - Integer.parseInt(start[3]);
+		int days = Integer.parseInt(end[0]) - Integer.parseInt(start[0]);
+		int months = Integer.parseInt(end[1]) - Integer.parseInt(start[1]);
+		int years = Integer.parseInt(end[2]) - Integer.parseInt(start[2]);
+		
+		if(start[2].equals(end[2])) { //same year
+			if(start[1].equals(end[1])) { //same month
+				if(start[0].equals(end[0])) { //same day
+					if(start[3].equals(end[3])) { //same hour
+						if(start[4].equals(end[4])) { //same minute
+							return "0 minutes";
+						}
+						return minutes + " minutes";
+					}
+					return hours + " hours";
+				}
+			}
+		}
+		
+		return ""; //to be completed
 	}
 }
--- src/assignment1/AnalyticsTest.java
+++ src/assignment1/AnalyticsTest.java
@@ -1,135 +1,180 @@
-package assignment1;
+//DO NOT MODIFY THIS FILE
 
-import static org.junit.jupiter.api.Assertions.*;
+package assignment1;
 
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import org.junit.rules.*;
+import java.lang.reflect.*;
 
-class AnalyticsTest {
-	private Analytics a1, a2;
-	
-	@BeforeEach
-	void setup() throws FileNotFoundException {
-		a1 = new Analytics("diagnostic_2019.csv");
-		a2 = new Analytics("diagnostic_2020.csv");
-	}
-	
-	@Test
-	void testCountQuestions() {
-		assertEquals(32, a1.countQuestions());
-		assertEquals(31, a2.countQuestions());
-	}
+public class AnalyticsTest {
+	private Analytics diagnostic2018, diagnostic2019, diagnostic2020;
+	public static String currentMethodName = null;
+	public static int score = 0;
+	public static String result = "";
 
-	@Test
-	void testGetQuestionWeight() {
-		assertEquals(5, a1.getQuestionWeight(1));
-		assertEquals(2, a1.getQuestionWeight(2));
-		assertEquals(1, a1.getQuestionWeight(20));
-		assertEquals(1, a1.getQuestionWeight(32));
-		assertEquals(0, a1.getQuestionWeight(0));
-		assertEquals(0, a1.getQuestionWeight(33));
+	@Before
+	public void beforeEachTest() throws FileNotFoundException {
+		currentMethodName = null;
+		diagnostic2018 = new Analytics("diagnostic_2018.csv");
+		diagnostic2019 = new Analytics("diagnostic_2019.csv");
+		diagnostic2020 = new Analytics("diagnostic_2020.csv");
 	}
 
-	@Test
-	void testQuizTotal() {
-		assertEquals(41, a1.quizTotal());
-		assertEquals(40, a2.quizTotal());	
-	}
-	
-	@Test
-	void testAverageQuizPercentageMark() {
-		assertEquals(74.5858, a1.averageQuizPercentageMark(), 0.001);
-		assertEquals(71.8073, a2.averageQuizPercentageMark(), 0.001);	
+	@Test 
+	public void testCountQuestions() {
+		assertEquals(2, diagnostic2018.countQuestions());
+		assertEquals(32, diagnostic2019.countQuestions());
+		assertEquals(31, diagnostic2020.countQuestions());
+		currentMethodName = new Throwable().getStackTrace()[0].getMethodName();
 	}
 
-	@Test
-	void testHighestMark() {
-		assertEquals(41, a1.highestMark());
-		assertEquals(40, a2.highestMark());	
+	@Test 
+	public void testQuizTotal() {
+		assertEquals(41, diagnostic2019.quizTotal());
+		assertEquals(40, diagnostic2020.quizTotal());	
+		currentMethodName = new Throwable().getStackTrace()[0].getMethodName();
 	}
 
-	@Test
-	void testCountPasses() {
-		assertEquals(513, a1.countPasses(20));
-		assertEquals(260, a2.countPasses(20));		
-		assertEquals(452, a1.countPasses(25));
-		assertEquals(223, a2.countPasses(25));		
-		assertEquals(357, a1.countPasses(30));
-		assertEquals(160, a2.countPasses(30));		
-		assertEquals(189, a1.countPasses(35));
-		assertEquals(78, a2.countPasses(35));		
+	@Test 
+	public void testGetQuestionWeight() {
+		assertEquals(3, diagnostic2018.getQuestionWeight(1));
+		assertEquals(3, diagnostic2018.getQuestionWeight(1));
+		assertEquals(5, diagnostic2019.getQuestionWeight(1));
+		assertEquals(2, diagnostic2019.getQuestionWeight(2));
+		assertEquals(1, diagnostic2019.getQuestionWeight(20));
+		assertEquals(1, diagnostic2019.getQuestionWeight(32));
+		assertEquals(0, diagnostic2019.getQuestionWeight(0));
+		assertEquals(0, diagnostic2019.getQuestionWeight(33));
+		assertEquals(5, diagnostic2020.getQuestionWeight(1));
+		assertEquals(2, diagnostic2020.getQuestionWeight(2));
+		assertEquals(1, diagnostic2020.getQuestionWeight(18));
+		currentMethodName = new Throwable().getStackTrace()[0].getMethodName();
 	}
 
-	@Test
-	void testBestMarkByStudent() {
-		assertEquals(41, a1.bestMarkByStudent(2281102));
-		assertEquals(0, a1.bestMarkByStudent(1234567));
-		assertEquals(27, a1.bestMarkByStudent(2270809));
-		assertEquals(30, a1.bestMarkByStudent(2124406));
-		assertEquals(33, a2.bestMarkByStudent(602961));
+	@Test 
+	public void testCountPasses() {
+		assertEquals(586, diagnostic2019.countPasses(0));
+		assertEquals(27, diagnostic2019.countPasses(41));
+		assertEquals(314, diagnostic2020.countPasses(0));
+		assertEquals(14, diagnostic2020.countPasses(40));
+		assertEquals(517, diagnostic2019.countPasses(20));
+		assertEquals(264, diagnostic2020.countPasses(20));		
+		assertEquals(470, diagnostic2019.countPasses(25));
+		assertEquals(228, diagnostic2020.countPasses(25));		
+		assertEquals(389, diagnostic2019.countPasses(30));
+		assertEquals(177, diagnostic2020.countPasses(30));		
+		assertEquals(220, diagnostic2019.countPasses(35));
+		assertEquals(98, diagnostic2020.countPasses(35));		
+		currentMethodName = new Throwable().getStackTrace()[0].getMethodName();
 	}
 
-	@Test
-	void testAverageMarkByStudent() {
-		assertEquals(35.6666, a1.averageMarkByStudent(2281102), 0.001);
-		assertEquals(0, a1.averageMarkByStudent(1234567), 0.001);
-		assertEquals(27, a1.averageMarkByStudent(2270809), 0.001);
-		assertEquals(14.25, a1.averageMarkByStudent(2124406), 0.001);
-		assertEquals(24.75, a2.averageMarkByStudent(602961), 0.001);
+	@Test 
+	public void testAvgQuizPercent() {
+		assertEquals(74.5858, diagnostic2019.avgQuizPercent(), 0.001);
+		assertEquals(71.8073, diagnostic2020.avgQuizPercent(), 0.001);	
+		currentMethodName = new Throwable().getStackTrace()[0].getMethodName();
 	}
 
-	@Test
-	void testAveragePercentageMarkByQuestion() {
-		assertEquals(88.082, a1.averagePercentageMarkByQuestion(1), 0.001);
-		assertEquals(93.881, a1.averagePercentageMarkByQuestion(2), 0.001);
-		assertEquals(90.366, a1.averagePercentageMarkByQuestion(3), 0.001);
-		assertEquals(92.819, a1.averagePercentageMarkByQuestion(4), 0.001);
-		assertEquals(60.869, a1.averagePercentageMarkByQuestion(31), 0.001);
-		assertEquals(60.869, a1.averagePercentageMarkByQuestion(32), 0.001);
-		assertEquals(89.805, a2.averagePercentageMarkByQuestion(1), 0.001);
-		assertEquals(93.521, a2.averagePercentageMarkByQuestion(2), 0.001);
-		assertEquals(92.0, a2.averagePercentageMarkByQuestion(3), 0.001);
-		assertEquals(93.399, a2.averagePercentageMarkByQuestion(4), 0.001);
-		assertEquals(0, a1.averagePercentageMarkByQuestion(0), 0.001);
-		assertEquals(0, a2.averagePercentageMarkByQuestion(0), 0.001);
-		assertEquals(60.869, a1.averagePercentageMarkByQuestion(31), 0.001);
-		assertEquals(64.609, a2.averagePercentageMarkByQuestion(30), 0.001);
+	@Test 
+	public void testHighestMark() {
+		assertEquals(41, diagnostic2019.highestMark());
+		assertEquals(40, diagnostic2020.highestMark());	
+		currentMethodName = new Throwable().getStackTrace()[0].getMethodName();
 	}
 
-	@Test
-	void testHardestQuestion() {
-		assertEquals(28, a1.hardestQuestion());
-		assertEquals(22, a2.hardestQuestion());	
+	@Test 
+	public void testBestMarkByStudent() {
+		assertEquals(41, diagnostic2019.bestMarkByStudent(2281102));
+		assertEquals(0, diagnostic2019.bestMarkByStudent(1234567));
+		assertEquals(27, diagnostic2019.bestMarkByStudent(2270809));
+		assertEquals(30, diagnostic2019.bestMarkByStudent(2124406));
+		assertEquals(33, diagnostic2020.bestMarkByStudent(602961));
+		currentMethodName = new Throwable().getStackTrace()[0].getMethodName();
 	}
 
-	@Test
-	void testGetAllAttemptsForStudent() {
-		assertEquals("[26, 40, 41]", Arrays.toString(a1.getAllAttemptsForStudent(2281102)));
-		assertEquals("[]", Arrays.toString(a1.getAllAttemptsForStudent(1234567)));
-		assertEquals("[27]", Arrays.toString(a1.getAllAttemptsForStudent(2270809)));
-		assertEquals("[2, 8, 17, 30]", Arrays.toString(a1.getAllAttemptsForStudent(2124406)));
-		assertEquals("[14, 24, 28, 33]", Arrays.toString(a2.getAllAttemptsForStudent(602961)));
+	@Test 
+	public void testAvgMarkByStudentId() {
+		assertEquals(35.6666, diagnostic2019.avgMarkByStudentId(2281102), 0.001);
+		assertEquals(0, diagnostic2019.avgMarkByStudentId(1234567), 0.001);
+		assertEquals(27, diagnostic2019.avgMarkByStudentId(2270809), 0.001);
+		assertEquals(14.25, diagnostic2019.avgMarkByStudentId(2124406), 0.001);
+		assertEquals(24.75, diagnostic2020.avgMarkByStudentId(602961), 0.001);
+		currentMethodName = new Throwable().getStackTrace()[0].getMethodName();
 	}
 
-	@Test
-	void testGetHardestToEasiest() {
-		assertEquals("[28, 27, 29, 22, 30, 9, 31, 32, 24, 14, 12, 26, 21, 13, 11, 20, 19, 10, 25, 17, 23, 18, 1, 16, 3, 5, 4, 7, 2, 8, 15, 6]", Arrays.toString(a1.getHardestToEasiest()));
-		assertEquals("[22, 28, 27, 29, 9, 31, 30, 12, 26, 21, 14, 11, 24, 19, 13, 17, 10, 20, 25, 23, 16, 18, 5, 1, 7, 3, 8, 6, 4, 2, 15]", Arrays.toString(a2.getHardestToEasiest()));
+	@Test 
+	public void testAvgPercentByQuestion() {
+		assertEquals(47.133, diagnostic2018.avgPercentByQuestion(1), 0.001);
+		assertEquals(0, diagnostic2018.avgPercentByQuestion(2), 0.001);
+		assertEquals(88.082, diagnostic2019.avgPercentByQuestion(1), 0.001);
+		assertEquals(93.881, diagnostic2019.avgPercentByQuestion(2), 0.001);
+		assertEquals(90.366, diagnostic2019.avgPercentByQuestion(3), 0.001);
+		assertEquals(92.819, diagnostic2019.avgPercentByQuestion(4), 0.001);
+		assertEquals(60.869, diagnostic2019.avgPercentByQuestion(31), 0.001);
+		assertEquals(60.869, diagnostic2019.avgPercentByQuestion(32), 0.001);
+		assertEquals(89.805, diagnostic2020.avgPercentByQuestion(1), 0.001);
+		assertEquals(93.521, diagnostic2020.avgPercentByQuestion(2), 0.001);
+		assertEquals(92.0, diagnostic2020.avgPercentByQuestion(3), 0.001);
+		assertEquals(93.399, diagnostic2020.avgPercentByQuestion(4), 0.001);
+		assertEquals(0, diagnostic2019.avgPercentByQuestion(0), 0.001);
+		assertEquals(0, diagnostic2020.avgPercentByQuestion(0), 0.001);
+		assertEquals(0, diagnostic2019.avgPercentByQuestion(35), 0.001);
+		assertEquals(0, diagnostic2020.avgPercentByQuestion(40), 0.001);
+		assertEquals(0, diagnostic2019.avgPercentByQuestion(-2), 0.001);
+		assertEquals(0, diagnostic2020.avgPercentByQuestion(-3), 0.001);
+		assertEquals(60.869, diagnostic2019.avgPercentByQuestion(31), 0.001);
+		assertEquals(64.609, diagnostic2020.avgPercentByQuestion(30), 0.001);
+		currentMethodName = new Throwable().getStackTrace()[0].getMethodName();
 	}
 
-	@Test
-	void testGetTimeTakenAsStringByIndex() {
-		assertEquals("59 minutes", a1.getTimeTakenAsStringByIndex(0));
-		assertEquals("38 minutes", a1.getTimeTakenAsStringByIndex(1));
-		assertEquals("1 hour and 5 minutes", a1.getTimeTakenAsStringByIndex(40));
-		assertEquals("5 days, 18 hours and 29 minutes", a1.getTimeTakenAsStringByIndex(10));
-		assertEquals("18 days, 14 hours and 54 minutes", a2.getTimeTakenAsStringByIndex(10));
-		assertEquals("2 days and 3 hours", a2.getTimeTakenAsStringByIndex(14));
-		assertEquals("1 day", a2.getTimeTakenAsStringByIndex(23));
-		assertEquals("9 days", a2.getTimeTakenAsStringByIndex(27));
-		assertEquals("0 minutes", a2.getTimeTakenAsStringByIndex(53));
+	@Test 
+	public void testHardestQuestion() {
+		assertEquals(2, diagnostic2018.hardestQuestion());
+		assertEquals(28, diagnostic2019.hardestQuestion());
+		assertEquals(22, diagnostic2020.hardestQuestion());	
+		currentMethodName = new Throwable().getStackTrace()[0].getMethodName();
 	}
+
+	@Test 
+	public void testGetStudentAttempts() {
+		assertEquals("[26, 40, 41]", Arrays.toString(diagnostic2019.getStudentAttempts(2281102)));
+		assertEquals("[]", Arrays.toString(diagnostic2019.getStudentAttempts(1234567)));
+		assertEquals("[27]", Arrays.toString(diagnostic2019.getStudentAttempts(2270809)));
+		assertEquals("[2, 8, 17, 30]", Arrays.toString(diagnostic2019.getStudentAttempts(2124406)));
+		assertEquals("[14, 24, 28, 33]", Arrays.toString(diagnostic2020.getStudentAttempts(602961)));
+		currentMethodName = new Throwable().getStackTrace()[0].getMethodName();
+	}
+
+	@Test 
+	public void testGetHardestToEasiest() {
+		assertEquals("[2, 1]", Arrays.toString(diagnostic2018.getHardestToEasiest()));
+		assertEquals("[28, 27, 29, 22, 30, 9, 31, 32, 24, 14, 12, 26, 21, 13, 11, 20, 19, 10, 25, 17, 23, 18, 1, 16, 3, 5, 4, 7, 2, 8, 15, 6]", Arrays.toString(diagnostic2019.getHardestToEasiest()));
+		assertEquals("[22, 28, 27, 29, 9, 31, 30, 12, 26, 21, 14, 11, 24, 19, 13, 17, 10, 20, 25, 23, 16, 18, 5, 1, 7, 3, 8, 6, 4, 2, 15]", Arrays.toString(diagnostic2020.getHardestToEasiest()));
+		currentMethodName = new Throwable().getStackTrace()[0].getMethodName();
+	}
+
+	@Test 
+	public void testTimeToDisplayByIndex() {
+		assertEquals("59 minutes", diagnostic2019.timeToDisplayByIndex(0));
+		assertEquals("38 minutes", diagnostic2019.timeToDisplayByIndex(1));
+		assertEquals("1 hour and 5 minutes", diagnostic2019.timeToDisplayByIndex(40));
+		assertEquals("5 days, 18 hours and 29 minutes", diagnostic2019.timeToDisplayByIndex(10));
+		assertEquals("1 hour", diagnostic2019.timeToDisplayByIndex(2));
+		assertEquals("3 hours", diagnostic2019.timeToDisplayByIndex(3));
+		assertEquals("18 days, 14 hours and 54 minutes", diagnostic2020.timeToDisplayByIndex(10));
+		assertEquals("2 days and 3 hours", diagnostic2020.timeToDisplayByIndex(14));
+		assertEquals("1 day", diagnostic2020.timeToDisplayByIndex(23));
+		assertEquals("9 days", diagnostic2020.timeToDisplayByIndex(27));
+		assertEquals("0 minutes", diagnostic2020.timeToDisplayByIndex(53));
+		assertEquals("1 minute", diagnostic2020.timeToDisplayByIndex(4));
+		currentMethodName = new Throwable().getStackTrace()[0].getMethodName();
+	}
+
 }
